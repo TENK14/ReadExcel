a524eb57d78851d80d5e8773dc5b1deecce6d506
Tomáš Wagner <t.e.n.k@seznam.cz>
2017-09-24 19:23:27 +0200

    Print all Sheets name, and print all active range of these sheets.

--- ReadExcel/Program.cs
+++ ReadExcel/Program.cs
@@ -25,36 +25,50 @@ namespace ReadExcel
             //Create COM Objects. Create a COM object for everything that is referenced
             Excel.Application xlApp = new Excel.Application(); 
             Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(location); // D:\_tmp\excelSheet.xlsx
-            //Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"D:\_tmp\excelSheet.xlsx"); // D:\_tmp\excelSheet.xlsx
-            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
-            Excel.Range xlRange = xlWorksheet.UsedRange;
 
-            //iterate over the rows and columns and print to the console as it appears in the file
-            //excel is not zero based!!
-            for (int i = 1; i <= rowCount; i++)
+            //Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];//Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"D:\_tmp\excelSheet.xlsx"); // D:\_tmp\excelSheet.xlsx
+
+            foreach (Excel._Worksheet xlWorksheet in xlWorkbook.Sheets)
             {
-                for (int j = 1; j <= colCount; j++)
+                Console.WriteLine(new String('-', 20));
+                Excel.Range xlRange = xlWorksheet.UsedRange;
+
+                Console.WriteLine(xlWorksheet.Name);
+
+                //iterate over the rows and columns and print to the console as it appears in the file
+                //excel is not zero based!!
+
+                rowCount = xlRange.Rows.Count;
+                colCount = xlRange.Columns.Count;
+
+                for (int i = 1; i <= rowCount; i++)
                 {
-                    //new line
-                    if (j == 1)
+                    for (int j = 1; j <= colCount; j++)
                     {
-                        Console.Write("\r\n");
-                    }
-                    
-                    //write the value to the console
-                    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
-                    {
-                        Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
-                    }
-                    else
-                    {
-                        Console.Write("\t");
-                    }
+                        //new line
+                        if (j == 1)
+                        {
+                            Console.Write("\r\n");
+                        }
 
-                    //add useful things here!   
+                        //write the value to the console
+                        if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                        {
+                            Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
+                        }
+                        else
+                        {
+                            Console.Write("\t");
+                        }
+
+                        //add useful things here!   
+                    }
                 }
-            }
+                Console.WriteLine();
 
+                Marshal.ReleaseComObject(xlRange);
+                Marshal.ReleaseComObject(xlWorksheet);
+            }
             //cleanup
             GC.Collect();
             GC.WaitForPendingFinalizers();
@@ -64,8 +78,8 @@ namespace ReadExcel
             //  ex: [somthing].[something].[something] is bad
 
             //release com objects to fully kill excel process from running in the background
-            Marshal.ReleaseComObject(xlRange);
-            Marshal.ReleaseComObject(xlWorksheet);
+            //Marshal.ReleaseComObject(xlRange);
+            //Marshal.ReleaseComObject(xlWorksheet);
 
             //close and release
             xlWorkbook.Close();
